b6047f0b0de5db96a2f10310f256d40d78805635
Sam Pritchard <sampritchard0801@gmail.com>
2025-10-05 04:56:28 +0100

    fix runtime config issue

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import { resolve } from 'node:path'
 import adze from 'adze'
 import { OBSWebSocket } from 'obs-websocket-js'
-import rawConfig from '../config.toml'
 import { ConfigSchema } from '../types/config'
 import { fsProcessCell, updateSource } from './updater'
 import { fetchSheetData, getBoundSources, requestUri } from './utils'
 
+const configPath = resolve(process.cwd(), 'config.toml')
+const configFile = Bun.file(configPath)
+const rawConfig = Bun.TOML.parse(await configFile.text())
 const config = ConfigSchema.parse(rawConfig)
 const wsEnabled = config.obs?.enabled ?? false
 const fsEnabled = config.fs?.enabled ?? false
@@ -27,6 +30,9 @@ if (wsEnabled) {
   const host = config.obs?.host ?? 'localhost'
   const port = config.obs?.port ?? 4455
   const password = config.obs?.password
+
+  //adze.namespace('DEBUG').alert(`Connecting to OBS WebSocket server at ws://${host}:${port} WITH password ${password ? 'set' : 'NOT set'}`)
+
   await obs.connect(`ws://${host}:${port}`, password)
   adze.namespace('startup').success('Connected to OBS WebSocket server.')
 }
